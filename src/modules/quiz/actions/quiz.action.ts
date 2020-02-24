ad835a439a0c713191bb8e6384f69b18f2b6a380
Elvio Cavalcante <elviocb@gmail.com>
2020-02-24 19:32:10 -0300

    chore:  add call to load questions into action

--- src/modules/quiz/actions/quiz.action.ts
+++ src/modules/quiz/actions/quiz.action.ts
@@ -1,13 +1,18 @@
 import { Dispatch } from "redux";
 import actionTypes from "../constants/quiz.action-type";
+import quizProvider from "../../../sdk/lib/quiz/quiz.provider";
 
 export const loadAllQuestions = () => async (dispatch: Dispatch) => {
   dispatch({ type: actionTypes.LOAD_ALL_QUESTIONS });
 
-  const ok = true;
-
-  if (ok) {
-    dispatch(loadAllQuestionsSuccess({}));
+  const { ok, data } = await quizProvider.getQuestions({
+    amount: 10,
+    difficulty: "hard",
+    type: "boolean",
+  });
+  console.log("data", data);
+  if (ok && data) {
+    dispatch(loadAllQuestionsSuccess(data.results));
   } else {
     dispatch(fetchEditSalePaymentsFail());
   }

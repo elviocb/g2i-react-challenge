8823fdae91a0ec3f0e7e4bfbb744257c43909f76
Elvio Cavalcante <elviocb@gmail.com>
2020-02-24 20:57:42 -0300

    chore: add router to the main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
 import configureStore from "./store/store";
+import QuizPage from "./modules/quiz/components/quiz.page";
 const store = configureStore();
 
 const App: React.FC<{}> = () => {
   return (
     <div className="App">
-      <Provider store={store}>Cool text here</Provider>
+      <Provider store={store}>
+        <Router basename={process.env.REACT_APP_ROUTER_BASENAME}>
+          <QuizPage />
+        </Router>
+      </Provider>
     </div>
   );
 };

8f32fa812e4ea7e227f1089a0144ddb65f1b61c2
Elvio Cavalcante <elviocb@gmail.com>
2020-02-25 17:58:58 -0300

    chore: set clean beackground color

--- src/modules/quiz/components/background.component.tsx
+++ src/modules/quiz/components/background.component.tsx
@@ -1,17 +1,15 @@
+import React from "react";
 import styled from "styled-components";
-import image from "../../../assets/images/bg.jpg";
 
 const BackgroundImage = styled.div`
-  background-image: url(${image}),
-    -moz-radial-gradient(center, ellipse cover, #2b313b 0%, #111720 100%);
-  background-image: url(${image}),
-    -webkit-radial-gradient(center, ellipse cover, #2b313b 0%, #111720 100%);
-  background-image: url(${image}), radial-gradient(ellipse at center, #34445e 0%, #111720 100%);
-  background-blend-mode: luminosity;
+  position: absolute;
+  background-image: -webkit-radial-gradient(center, ellipse cover, #2b313b 0%, #111720 100%);
   background-repeat: repeat-y;
   background-size: cover;
+  height: 100%;
+  width: 100%;
   min-width: 100%;
   min-height: 100%;
 `;
 
-export default BackgroundImage;
+export default React.memo(BackgroundImage);

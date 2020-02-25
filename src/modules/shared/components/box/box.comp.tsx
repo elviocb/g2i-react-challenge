1b1836ca245a94a3283effe14b5dde3ba71ebb5d
Elvio Cavalcante <elviocb@gmail.com>
2020-02-25 17:58:05 -0300

    feat: extend box and typography props

--- src/modules/shared/components/box/box.comp.tsx
+++ src/modules/shared/components/box/box.comp.tsx
@@ -1,19 +1,37 @@
 import React, { FC } from "react";
 
-import { SpaceProps, BackgroundColorProps, BorderProps, FlexboxProps } from "styled-system";
+import {
+  SpaceProps,
+  BackgroundColorProps,
+  BorderProps,
+  FlexboxProps,
+  flexbox,
+  layout,
+  LayoutProps,
+} from "styled-system";
 import styled from "styled-components";
 
 import { space } from "styled-system";
 
 const BoxContainer = styled.div`
+  ${flexbox};
   ${space};
+  ${layout};
 `;
 
 type BoxBaseProps = {
   children: any;
+  width?: string | number;
+  height?: string | number;
 };
 
-type BoxProps = SpaceProps & FlexboxProps & BackgroundColorProps & BorderProps & BoxBaseProps;
+type BoxProps = SpaceProps &
+  FlexboxProps &
+  BackgroundColorProps &
+  BorderProps &
+  LayoutProps &
+  BoxBaseProps &
+  React.HTMLAttributes<HTMLDivElement>;
 
 const Box: FC<BoxProps> = ({ children, ...props }: BoxProps) => {
   return <BoxContainer {...props}>{children}</BoxContainer>;
--- src/modules/shared/components/typography/typography.component.tsx
+++ src/modules/shared/components/typography/typography.component.tsx
@@ -1,41 +1,40 @@
 import React, { FC } from "react";
 import styled from "styled-components";
 import { theme, ThemeColorNames } from "../../theme";
+import { typography, TypographyProps, SpaceProps, space } from "styled-system";
 
 type TypographyTag = "h1" | "h2" | "h3" | "h4" | "h5" | "p" | "span";
 
 const baseFont = `'Montserrat', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'`;
 
-interface TypographyProps {
+interface TypographyCompProps
+  extends TypographyProps,
+    SpaceProps,
+    React.HTMLAttributes<HTMLHeadingElement> {
   color?: ThemeColorNames;
-  fontSize?: number;
   fontWeight?: number;
   lineHeight?: number;
   tag?: TypographyTag;
 }
 
-const Typography: FC<TypographyProps> = ({
-  tag,
-  fontSize,
-  fontWeight,
-  lineHeight,
-  color,
-  children,
-}) => {
-  const TagComponent = styled(tag || "p")<TypographyProps>`
+const Typography: FC<TypographyCompProps> = (props) => {
+  const { tag, fontWeight, lineHeight, color, children } = props;
+
+  const TagComponent = styled(tag || "p")<TypographyCompProps>`
     margin: 0;
     padding: 0;
 
-    ${(props: TypographyProps) =>
+    ${(props: TypographyCompProps) =>
       `font-family: ${baseFont};
-        font-size: ${fontSize || 14}px;
         font-weight: ${fontWeight || 500};
         line-height: ${lineHeight || 1.5};
         color: ${color ? theme.colors[color] : theme.colors.black};
         `};
+    ${typography}
+    ${space}
   `;
 
-  return <TagComponent>{children}</TagComponent>;
+  return <TagComponent {...props}>{children}</TagComponent>;
 };
 
 export default Typography;

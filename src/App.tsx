b87bb418d021d3a3e1a46361a028348446f1e45d
Elvio Cavalcante <elviocb@gmail.com>
2020-02-25 20:34:08 -0300

    chore: remove background image

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,15 @@ import "./App.css";
 
 import configureStore from "./store/store";
 import QuizPage from "./modules/quiz/components/quiz.page";
-import Background from "./modules/quiz/components/background.component";
 const store = configureStore();
 
 const App: React.FC<{}> = () => {
   return (
     <div className="App">
       <Provider store={store}>
-        <Background>
-          <Router basename={process.env.REACT_APP_ROUTER_BASENAME}>
-            <QuizPage />
-          </Router>
-        </Background>
+        <Router basename={process.env.REACT_APP_ROUTER_BASENAME}>
+          <QuizPage />
+        </Router>
       </Provider>
     </div>
   );
